23ccffc3ae561889daa9e65aa6e34290680bfde0
Nicholas <nmd37@njit.edu>
2026-04-09 14:15:03 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    /* 'public static void main...' is a METHOD.  */
+    public static void main(String[] args){
+        /* 'public' is an access modifier
+        'static' means that you cannot alter anything in this method. */ 
+
+        /* 'args' is short for 'arguments' */
+        
+        System.out.println("Hello, World!");
+    }
+}
